002365f12cabf5ba3793625457ea9c5149369407
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 23:27:43 -0600

    solve

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-# Write your AC Load Estimator solution here
+width = float(input("how wide is the room? "))
+height = float(input("how tall is the room? "))
+number_of_people = int(input("how many people in the room? "))
+
+print(round(width * height *  0.1 + number_of_people * 0.06, 2))
