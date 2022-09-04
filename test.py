856a3c92c5ce2841990c62f264cbfc00f11e30ba
Rob Cobb <rwcobbjr@gmail.com>
2022-09-04 17:29:16 -0600

    fix tests

--- test.py
+++ test.py
@@ -8,14 +8,14 @@ class Test(TestCase):
     @patch('builtins.input', return_value="50")
     def test_50_USD(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("50.00 USD is 20529.50 NGN")
+        mock_print.assert_called_with("50.00 USD is 21227.50 NGN")
         sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', return_value="14.01")
     def test_14_01_USD(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("14.01 USD is 5752.37 NGN")
+        mock_print.assert_called_with("14.01 USD is 5947.95 NGN")
         sys.modules.pop('main')
 
 if __name__ == '__main__':
