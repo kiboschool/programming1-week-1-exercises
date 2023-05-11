32c41105a66e372cb67c7f529d31bbf121649b19
josiahx <jnyangaga@multiply.cloud>
2023-05-11 05:32:14 +0400

    Add grade weights

--- test_main.py
+++ test_main.py
@@ -3,9 +3,13 @@ from unittest import TestCase
 import unittest
 import sys
 
+from gradescope_utils.autograder_utils.decorators import weight
+
+
 class Test(TestCase):
     @patch('builtins.print')
     @patch('builtins.input', side_effect=["12", "15", "4"])
+    @weight(10)
     def test_12x15x4(self, mock_input, mock_print):
         import main
         try:
@@ -15,6 +19,7 @@ class Test(TestCase):
 
     @patch('builtins.print')
     @patch('builtins.input', side_effect=["100", "100", "100"])
+    @weight(10)
     def test_100x100x100(self, mock_input, mock_print):
         import main
         try:
